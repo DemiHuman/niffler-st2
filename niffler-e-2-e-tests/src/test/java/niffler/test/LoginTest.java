9f5476151288503150ba55e90ac9447c85b3f9d5
Pavel <eropkinp@gmail.com>
2023-05-08 22:48:35 +0300

    Немного доработан тест на обновление профайла пользователя

--- niffler-e-2-e-tests/src/test/java/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/LoginTest.java
@@ -1,7 +1,6 @@
 package niffler.test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -17,12 +16,14 @@ import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class LoginTest extends BaseWebTest {
 
   private static final String USER_DATA_BASE_URL = "http://127.0.0.1:8089";
+  private static final String NIFFLER_BASE_URL = "http://127.0.0.1:3000";
 
   @ValueSource(strings = {
       "testdata/dima.json",
@@ -47,10 +48,29 @@ public class LoginTest extends BaseWebTest {
   })
   @AllureId("501")
   @ParameterizedTest
-  public void updateUserInfoTest(@ClasspathUser UserJson user) throws IOException {
+  public void updateUserProfileTest(@ClasspathUser UserJson user) throws IOException {
+    step("Update user profile", () -> {
       UserService userService = retrofitInit(USER_DATA_BASE_URL, UserService.class);
       Response response = userService.updateUserInfo(user).execute().raw();
       assertEquals(200, response.code(), "Update failed");
+    });
+
+    step("Login", () -> {
+      open(NIFFLER_BASE_URL);
+      $("a[href*='redirect']").click();
+      $("input[name='username']").setValue(user.getUsername());
+      $("input[name='password']").setValue(user.getPassword());
+      $("button[type='submit']").click();
+    });
+
+    step("Go to user profile", () -> {
+      open(NIFFLER_BASE_URL + "/profile");
+    });
+    step("Checking user data profile", () -> {
+      $(By.name("firstname")).shouldHave(value(user.getFirstname().toUpperCase()));
+      $(By.name("surname")).shouldHave(value(user.getSurname().toUpperCase()));
+      $(".select-wrapper").shouldHave(text(user.getCurrency().toString()));
+    });
   }
 
   private static <T> T retrofitInit(String baseUrl, Class<T> service) {
